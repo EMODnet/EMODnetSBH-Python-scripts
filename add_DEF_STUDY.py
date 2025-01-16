d47b9f313096e1013381a517e5f5610c66daa75f
Helen Lillis <helen.lillis@jncc.gov.uk>
2025-01-16 09:45:36 +0000

    Update add_DEF_STUDY.py

--- add_DEF_STUDY.py
+++ add_DEF_STUDY.py
@@ -1,7 +1,7 @@
 ##########################
 ## Designed to work in Arcgis 10.1
 ## Creates Study Area polygons for all shapefiles within a chosen directory.
-## See https://www.emodnet-seabedhabitats.eu/contribute-data/data-exchange-format/#h1367596acc5c4544855643f4bc3cfd2a for written guidance on the data schema
+## See https://emodnet.ec.europa.eu/sites/emodnet.ec.europa.eu/files/public/Data%20Preparation%20Guide%20for%20Point%20Datasets%20v1_2.pdf for written guidance on the data schema
 ## Outputs Study Area features into a subfolder named "StudyAreas".
 ## Names shapefiles correctly.
 ## Collects GUI from original habitat map name and transfers across.
@@ -10,7 +10,7 @@
 ## Warning: The habitat maps contained within the supplied folder MUST be in a MESH DEF for this script to work.
 ## Created by: Graeme Duncan, JNCC for EMODnet Seabed Habitats 2014.
 ## Updated by JNCC 2019-12-19: removal of SUM_CONF field (it's been moved to the translated habitat DEF)
-## Contact: https://www.emodnet-seabedhabitats.eu/helpdesk/contact-us/
+## Contact: helpdesk@emodnet.ec.europa.eu
 ###########################
 import arcpy
 from arcpy import env
